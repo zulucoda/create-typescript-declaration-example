2b70f05baba2852bd906d814da6ab02c43b6f842
Muzikayise Flynn Buthelezi (zuluCoda) <muzi@mfbproject.co.za>
2020-07-24 22:02:26 +0200

    adding communicator-modular-cjs

---
+++ communicator-modular-cjs/communicator-modular-cjs.js
@@ -0,0 +1,11 @@
+/**
+ * common js example
+ */
+
+const communicatorModularCJS = {};
+
+communicatorModularCJS.greet = function (message) {
+    return `<h1> ${message} </h1>`;
+}
+
+module.exports = communicatorModularCJS;
