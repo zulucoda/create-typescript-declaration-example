a374a5d134bf0e5bfa1c12ec0376192c9666902e
Muzikayise Flynn Buthelezi (zuluCoda) <muzi@mfbproject.co.za>
2020-07-24 22:30:52 +0200

    calling communicator-global from ts file using declaration

--- app/communicator.ts
+++ app/communicator.ts
@@ -1,14 +1,15 @@
-import * as _ from 'lodash';
+// import * as _ from 'lodash';
 
 class Communicator {
     constructor() {}
 
     greet(message: string) {
-        return `<h1>${ _.toUpper(message) }</h1>`;
+        // return `<h1>${ _.toUpper(message) }</h1>`;
+        return communicatorGlobal(message);
     }
 }
 
 const communicator = new Communicator();
-document!.body.innerHTML = communicator.greet('Hello, world');
+document!.body.innerHTML = communicator.greet('Hello, world for global');
 
 
